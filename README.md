fabc1327f8a3f475b7e0e9bea7f87fc3e60121a7
Sameh Bilal <sameh.bilal@outlook.com>
2021-11-10 19:48:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Weather-Journal App Project
+
+## Overview
+This is project to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+## Instructions
+To get the project running, do as below:
+
+1. make sure that the Node and packages (express, cors and body-parser) installed.
+
+2. Start server by running the command `node server.js` in your terminal
+
+3. Insert Zip Code and feeling then the result will come out.
+
+
+### Final Output
+![Output](https://github.com/SamehBilal/weather-journal-app/blob/main/test.png)
---
+++ website/app.js
@@ -0,0 +1,90 @@
+/* Global Variables */
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'b2c132dfd80a3ac53d3246bcdd15b827'; // API Key for OpenWeatherMap
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', generateAction);
+
+/* Function called by event listener */
+function generateAction(e) {
+    e.preventDefault();
+
+    //get zip code & feeling
+    const zipCode = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+
+    if (zipCode !== '') {
+        getWeatherData(baseUrl, zipCode, apiKey)
+            .then(function(data) {
+                // add data to POST request
+                postData('/add', { temp: data.main.temp, date: newDate, content: content });
+            }).then(function() {
+                updateUI() // call updateUI to update browser content
+            }).catch(function(error) {
+                console.log(error);
+                alert(error);
+            });
+    } else {
+        console.log('Some inputs needs to be filled first!');
+        alert('Some inputs needs to be filled first!')
+    }
+
+
+}
+/* Function to GET Web API Data*/
+const getWeatherData = async(baseUrl, zipCode, apiKey) => {
+    // res equals to the result of fetch function
+    const res = await fetch(`${baseUrl}?q=${zipCode}&appid=${apiKey}`);
+    try {
+        // data equals to the result of fetch function
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+/* Function to POST data */
+const postData = async(url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            temp: data.temp,
+            date: data.date,
+            content: data.content
+        })
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+/* Function to GET Project Data */
+const updateUI = async() => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        console.log(allData);
+        // update new entry values
+        if (allData.temp !== undefined ) {
+            document.getElementById('date').innerHTML = 'Date: '+allData.date;
+            document.getElementById('temp').innerHTML = 'Temp: '+allData.temp ;
+            document.getElementById('content').innerHTML = 'Feeling: '+allData.content;
+        }
+    } catch (error) {
+        console.log('error', error);
+    }
+};
